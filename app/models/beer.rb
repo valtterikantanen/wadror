cbb488af3992589b29b73e77350714d6ad3e3b10
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-03 16:24:44 +0200

    Show average rating of beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    ratings.reduce(0){ |sum, rating| sum + rating.score }.to_f / ratings.count
+  end
 end
