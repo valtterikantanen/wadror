6bd8f153eb71fb96961fea670f7ec61b344d06a6
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-03 15:07:56 +0200

    Add model for ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "breweries#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "ratings", to: "ratings#index"
 end
