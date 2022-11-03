1a86e2e9da4c557ac56b4ea2e0bd64b61fd7d421
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-03 16:55:26 +0200

    Add a form for creating new ratings

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "ratings", to: "ratings#index"
+  post "ratings", to: "ratings#create"
+  get "ratings/new", to: "ratings#new"
 end
