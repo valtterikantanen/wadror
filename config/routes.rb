2d0e54d0608e4af8fe0448b5ca2f7f8c54c58615
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-03 19:30:39 +0200

    Add a button for deleting ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  resources :ratings, only: [:index, :new, :create]
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   root "breweries#index"
