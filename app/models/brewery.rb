e70ae53684fde43052cd5cffef39705cd8530dc2
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-03 20:46:31 +0200

    Add average rating for breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    ratings.reduce(0){ |sum, rating| sum + rating.score }.to_f / ratings.count
+  end
 end
