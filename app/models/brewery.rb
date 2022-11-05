2b0bbf1fbd061c868d7ecb31f003eb65d9d80676
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-05 22:31:38 +0200

    Add more validation for beers, breweries and users

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,4 +3,7 @@ class Brewery < ApplicationRecord
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1040, less_than_or_equal_to: 2022, only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include RatingAverage
 
-  validates :username, uniqueness: true, length: { minimum: 3 }
+  validates :username, uniqueness: true, length: { in: 3..30 }
 
   has_many :ratings
 end
