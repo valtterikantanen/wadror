daf119151367072efbfd70877b0697ad77773f2e
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-05 20:42:28 +0200

    Add routes, views and controller for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   # Defines the root path route ("/")
   root "breweries#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "signup", to: "users#new"
+  get "signin", to: "sessions#new"
+  delete "signout", to: "sessions#destroy"
 end
