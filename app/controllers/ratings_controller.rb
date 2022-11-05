71d2aa80e5e21c21874748c791ac196ced84444f
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-05 22:18:15 +0200

    Rating score must be an integer between 1 and 50

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,10 +9,15 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.new params.require(:rating).permit(:score, :beer_id)
-    rating.user = current_user
-    rating.save
-    redirect_to current_user
+    @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    @rating.user = current_user
+
+    if @rating.save
+      redirect_to user_path current_user
+    else
+      @beers = Beer.all
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
