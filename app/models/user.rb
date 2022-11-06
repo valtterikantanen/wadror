eda0fbed2aa9b4b6af34b418c77446d55e811a10
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-06 04:31:27 +0200

    Remove orphaned records after destroying a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, length: { in: 3..30 }
   validates :password, length: { minimum: 4 }, format: { with: /(?=.*\d)(?=.*[A-Z])/ }
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 end
