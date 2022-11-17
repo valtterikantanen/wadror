7d675c03b99e8117da8ec4acf24b780f1f647afe
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-17 19:29:57 +0200

    Add favorite beer style to user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
   has_many :beers, through: :ratings
   has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
+
+  def favorite_beer
+    return nil if ratings.empty?
+
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_style
+    return nil if ratings.empty?
+
+    average_scores = Beer.joins(:ratings).select("AVG(ratings.score) as average_score, style").group("style")
+
+    average_scores.max_by(&:average_score).style
+  end
 end
