60073bcab07ce96d8e3592e24d4ad3f76a41a588
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-11-18 00:17:27 +0200

    Make another attempt to fix the link issue

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -52,7 +52,7 @@ describe "Rating" do
 
   it "when deleting own rating it is removed" do
     visit user_path(user)
-    click_link("", href: "/ratings/1")
+    click_link("", href: "/ratings/#{user.ratings.first.id}")
 
     expect(Rating.count).to eq(4)
     expect(page).to have_content "Has made 3 ratings"
