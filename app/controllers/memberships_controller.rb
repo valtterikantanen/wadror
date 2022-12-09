974f603efe9f99064ccfcbd1fce46f68418fa389
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-09 21:15:30 +0200

    Redirect to beer club page after joining

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -28,7 +28,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to beer_clubs_path, notice: "Membership was successfully created." }
+        format.html { redirect_to beer_club_url(@membership.beer_club), notice: "#{current_user.username} welcome to the club." }
         format.json { render :show, status: :created, location: @membership }
       else
         @beer_clubs = BeerClub.all
