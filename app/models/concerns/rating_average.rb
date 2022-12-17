6adc8ab550fcda3b629d66c25ccd8a842747777f
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-17 06:50:33 +0200

    Fix average_rating function to return 0 if there are no ratings

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,6 +2,8 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
+    return 0.0 if ratings.empty?
+
     ratings.reduce(0){ |sum, rating| sum + rating.score }.to_f / ratings.count
   end
 end
