0d60516597d8b2c1f7e3d3ec1809a07144ac8fdc
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-18 23:38:37 +0200

    Add status to breweries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_005208) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_18_213241) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_005208) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
   end
 
   create_table "memberships", force: :cascade do |t|
