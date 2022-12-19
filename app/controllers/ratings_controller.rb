458b351be05106d59846516e90315980b4ceca61
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-19 02:05:34 +0200

    Display best beers, breweries, styles, most active users and recent ratings on ratings page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,11 @@
 class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
+    @recent_ratings = Rating.recent
+    @top_breweries = Brewery.top 3
+    @top_beers = Beer.top 3
+    @top_styles = Style.top 3
+    @top_users = User.top 3
   end
 
   def new
