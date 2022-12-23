99666c88d468228a4d381a513ee910ab68d1fb32
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-23 15:25:59 +0200

    Add column for admin status in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_213241) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_132220) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_213241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
   end
 
 end
