bb6314c8e2ece27f59e59611945825c8981ff4d1
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-24 10:20:34 +0200

    Add method for checking if current user is admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def ensure_that_signed_in
     redirect_to signin_path, notice: "You should be signed in" if current_user.nil?
   end
+
+  def ensure_that_user_is_admin
+    redirect_to root_path, notice: "You need to be an admin to perform this action" unless current_user&.admin
+  end
 end
