f19a9737d3af8cd9bac24c690691a76b116fe913
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-26 11:38:15 +0200

    Use caching for listing of all beers

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -6,11 +6,12 @@ class BeersController < ApplicationController
 
   # GET /beers or /beers.json
   def index
+    @order = params[:order] || "name"
+    return if request.format.html? && fragment_exist?("beerlist-#{@order}")
+
     @beers = Beer.includes(:brewery, :style, :ratings).all
 
-    order = params[:order] || "name"
-
-    @beers = case order
+    @beers = case @order
              when "name" then @beers.sort_by(&:name)
              when "brewery" then @beers.sort_by { |b| b.brewery.name }
              when "style" then @beers.sort_by { |b| b.style.name }
@@ -35,6 +36,8 @@ class BeersController < ApplicationController
 
   # POST /beers or /beers.json
   def create
+    %w[beerlist-name beerlist-brewery beerlist-style beerlist-rating].each{ |f| expire_fragment(f) }
+
     @beer = Beer.new(beer_params)
 
     respond_to do |format|
@@ -51,6 +54,8 @@ class BeersController < ApplicationController
 
   # PATCH/PUT /beers/1 or /beers/1.json
   def update
+    %w[beerlist-name beerlist-brewery beerlist-style beerlist-rating].each{ |f| expire_fragment(f) }
+
     respond_to do |format|
       if @beer.update(beer_params)
         format.html { redirect_to beer_url(@beer), notice: "Beer was successfully updated." }
@@ -64,6 +69,8 @@ class BeersController < ApplicationController
 
   # DELETE /beers/1 or /beers/1.json
   def destroy
+    %w[beerlist-name beerlist-brewery beerlist-style beerlist-rating].each{ |f| expire_fragment(f) }
+
     @beer.destroy
 
     respond_to do |format|
