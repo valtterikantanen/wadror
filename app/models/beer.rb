9896303c7ef88f76a2f7ba08a69021758b394e14
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-26 12:30:34 +0200

    Add fragment caching to brewery page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,7 @@
 class Beer < ApplicationRecord
   include RatingAverage
 
-  belongs_to :brewery
+  belongs_to :brewery, touch: true
   belongs_to :style
   has_many :ratings, dependent: :destroy
   has_many :raters, -> { distinct }, through: :ratings, source: :user
