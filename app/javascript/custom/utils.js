d34ec27b000a843d9af7dd3e6a6d9b59141eb76d
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-12-27 19:34:57 +0200

    Clean JavaScript code

--- app/javascript/custom/utils.js
+++ app/javascript/custom/utils.js
@@ -1,26 +1,22 @@
 const BEERS = {};
 const BREWERIES = {};
 
-const handleResponseBeers = (beers) => {
+const handleResponseBeers = beers => {
   BEERS.list = beers;
   BEERS.show();
 };
 
-const handleResponseBreweries = (breweries) => {
+const handleResponseBreweries = breweries => {
   BREWERIES.list = breweries;
   BREWERIES.show();
 };
 
-const createTableRow = (columns) => {
+const createTableRow = columns => {
   const tr = document.createElement("tr");
   tr.classList.add("tablerow");
-
-  columns.forEach((column) => {
-    appendChildToTableRow(tr, column);
-  });
-
+  columns.forEach(column => appendChildToTableRow(tr, column));
   return tr;
-}
+};
 
 const appendChildToTableRow = (tablerow, content) => {
   const td = tablerow.appendChild(document.createElement("td"));
@@ -28,14 +24,14 @@ const appendChildToTableRow = (tablerow, content) => {
 };
 
 const removeTableRows = () => {
-  document.querySelectorAll(".tablerow").forEach((el) => el.remove());
-}
+  document.querySelectorAll(".tablerow").forEach(el => el.remove());
+};
 
 BEERS.show = () => {
   removeTableRows();
   const table = document.getElementById("beertable");
 
-  BEERS.list.forEach((beer) => {
+  BEERS.list.forEach(beer => {
     const columns = [beer.name, beer.style.name, beer.brewery.name];
     const tr = createTableRow(columns);
     table.appendChild(tr);
@@ -46,101 +42,81 @@ BREWERIES.show = () => {
   removeTableRows();
   const table = document.getElementById("brewerytable");
 
-  BREWERIES.list.forEach((brewery) => {
+  BREWERIES.list.forEach(brewery => {
     const columns = [brewery.name, brewery.year, brewery.beers.amount, brewery.active ? "Active" : "Retired"];
     const tr = createTableRow(columns);
     table.appendChild(tr);
   });
 };
 
-BEERS.sortByName = () => {
-  BEERS.list.sort((a, b) => {
-    return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
-  });
-};
+const sortByName = arr => arr.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
+
+BEERS.sortByName = () => sortByName(BEERS.list);
+
+BREWERIES.sortByName = () => sortByName(BREWERIES.list);
 
 BEERS.sortByStyle = () => {
-  BEERS.list.sort((a, b) => {
-    return a.style.name.toUpperCase().localeCompare(b.style.name.toUpperCase());
-  });
+  BEERS.list.sort((a, b) => a.style.name.toUpperCase().localeCompare(b.style.name.toUpperCase()));
 };
 
 BEERS.sortByBrewery = () => {
-  BEERS.list.sort((a, b) => {
-    return a.brewery.name
-      .toUpperCase()
-      .localeCompare(b.brewery.name.toUpperCase());
-  });
+  BEERS.list.sort((a, b) => a.brewery.name.toUpperCase().localeCompare(b.brewery.name.toUpperCase()));
 };
 
-BREWERIES.sortByName = () => {
-  BREWERIES.list.sort((a, b) => {
-    return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
-  });
-};
+BREWERIES.sortByYear = () => BREWERIES.list.sort((a, b) => a.year - b.year);
 
-BREWERIES.sortByYear = () => {
-  BREWERIES.list.sort((a, b) => {
-    return a.year - b.year;
-  });
-};
+BREWERIES.sortByAmountOfBeers = () => BREWERIES.list.sort((a, b) => a.beers.amount - b.beers.amount);
 
-BREWERIES.sortByAmountOfBeers = () => {
-  BREWERIES.list.sort((a, b) => {
-    return a.beers.amount - b.beers.amount;
-  });
-};
+async function getData(filename) {
+  const response = await fetch(filename);
+  const data = await response.json();
+  if (filename === "beers.json") {
+    handleResponseBeers(data);
+  } else if (filename === "breweries.json") {
+    handleResponseBreweries(data);
+  }
+}
 
 const beers = () => {
   if (document.querySelectorAll("#beertable").length < 1) return;
 
-  document.getElementById("name").addEventListener("click", (e) => {
-    e.preventDefault();
-    BEERS.sortByName();
-    BEERS.show();
-  });
+  getData("beers.json");
 
-  document.getElementById("style").addEventListener("click", (e) => {
-    e.preventDefault();
-    BEERS.sortByStyle();
-    BEERS.show();
-  });
+  const callbacks = new Map([
+    ["name", BEERS.sortByName],
+    ["style", BEERS.sortByStyle],
+    ["brewery", BEERS.sortByBrewery]
+  ]);
 
-  document.getElementById("brewery").addEventListener("click", (e) => {
-    e.preventDefault();
-    BEERS.sortByBrewery();
-    BEERS.show();
-  });
+  const clickableElements = ["name", "style", "brewery"];
 
-  fetch("beers.json")
-    .then((response) => response.json())
-    .then(handleResponseBeers);
+  clickableElements.forEach(element => {
+    document.getElementById(element).addEventListener("click", () => {
+      callbacks.get(element)();
+      BEERS.show();
+    });
+  });
 };
 
 const breweries = () => {
   if (document.querySelectorAll("#brewerytable").length < 1) return;
 
-  document.getElementById("name").addEventListener("click", (e) => {
-    e.preventDefault();
-    BREWERIES.sortByName();
-    BREWERIES.show();
-  });
+  getData("breweries.json");
 
-  document.getElementById("year").addEventListener("click", (e) => {
-    e.preventDefault();
-    BREWERIES.sortByYear();
-    BREWERIES.show();
-  });
+  const callbacks = new Map([
+    ["name", BREWERIES.sortByName],
+    ["year", BREWERIES.sortByYear],
+    ["amount-of-beers", BREWERIES.sortByAmountOfBeers]
+  ]);
 
-  document.getElementById("amount-of-beers").addEventListener("click", (e) => {
-    e.preventDefault();
-    BREWERIES.sortByAmountOfBeers();
-    BREWERIES.show();
-  });
+  const clickableElements = ["name", "year", "amount-of-beers"];
 
-  fetch("breweries.json")
-    .then((response) => response.json())
-    .then(handleResponseBreweries);
+  clickableElements.forEach(element => {
+    document.getElementById(element).addEventListener("click", () => {
+      callbacks.get(element)();
+      BREWERIES.show();
+    });
+  });
 };
 
 export { beers, breweries };
